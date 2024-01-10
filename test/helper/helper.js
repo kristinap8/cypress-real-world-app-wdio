7862f9fbe8f11209662c480fb23e235df004d8bd
kristinap8 <58071369+kristinap8@users.noreply.github.com>
2024-01-10 11:35:14 +0100

    add make payment method

--- test/helper/helper.js
+++ test/helper/helper.js
@@ -1,6 +1,27 @@
+import loginPage from "../pageobjects/login.page.js";
+import navBarPage from "../pageobjects/navBar.page.js";
+import selectContactPage from "../pageobjects/selectContact.page.js";
+import paymentPage from "../pageobjects/payment.page.js";
+import sideMenu from '../pageobjects/sideMenu.page.js';
+import { generateTransactionData } from "./generator.js";
+import usersData from '../fixtures/users.json' assert {type: "json"};
+
 export function isReddishColor(rgba) {
     const rgbaColor = rgba.match(/\d+/g).map(Number);
     return rgbaColor[0] > 100 && rgbaColor[0] > rgbaColor[1] * 2 && rgbaColor[0] > rgbaColor[2] * 2;
+}
+
+export async function makePayment(payerInd, receiverInd) {
+    await loginPage.openPage();
+    await loginPage.login(usersData[payerInd].username, usersData[payerInd].password);
+
+    await navBarPage.clickNewTransactionBtn();
+    await selectContactPage.clickContactItem(receiverInd - 1);
+
+    await paymentPage.fillPaymentForm(generateTransactionData());
+    await paymentPage.clickPayBtn();
+
+    await sideMenu.clickLogoutMenuItem();
 }
 
 export function findPasswordByUsername(users, username) {
